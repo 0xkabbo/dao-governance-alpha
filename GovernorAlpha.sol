d2cebe97fcf87d7a3e9c769884e315332bc15f04
Kabbo <161012310+0xkabbo@users.noreply.github.com>
2026-02-25 21:11:24 +0600

    Create GovernorAlpha.sol

---
+++ GovernorAlpha.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/governance/Governor.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorSettings.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
+
+/**
+ * @title GovernorAlpha
+ * @dev Professional DAO governance contract using OpenZeppelin modular extensions.
+ */
+contract GovernorAlpha is 
+    Governor, 
+    GovernorSettings, 
+    GovernorCountingSimple, 
+    GovernorVotes, 
+    GovernorVotesQuorumFraction 
+{
+    constructor(IVotes _token)
+        Governor("DAO Governance Alpha")
+        GovernorSettings(1 /* 1 block */, 50400 /* ~1 week */, 0)
+        GovernorVotes(_token)
+        GovernorVotesQuorumFraction(4) // 4% quorum
+    {}
+
+    // The following functions are overrides required by Solidity.
+
+    function votingDelay()
+        public
+        view
+        override(Governor, GovernorSettings)
+        returns (uint256)
+    {
+        return super.votingDelay();
+    }
+
+    function votingPeriod()
+        public
+        view
+        override(Governor, GovernorSettings)
+        returns (uint256)
+    {
+        return super.votingPeriod();
+    }
+
+    function quorum(uint256 blockNumber)
+        public
+        view
+        override(Governor, GovernorVotesQuorumFraction)
+        returns (uint256)
+    {
+        return super.quorum(blockNumber);
+    }
+
+    function proposalThreshold()
+        public
+        view
+        override(Governor, GovernorSettings)
+        returns (uint256)
+    {
+        return super.proposalThreshold();
+    }
+}
